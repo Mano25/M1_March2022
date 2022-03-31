81b83c3030a73aacfbb7afa1f3c8a6e0aadc7fd6
Mano25 <101637522+Mano25@users.noreply.github.com>
2022-03-31 11:22:03 +0530

    first commit

---
+++ 3_implementation/inc/function.h
@@ -0,0 +1,45 @@
+#ifndef __FUNCTION_H__
+#define __FUNCTION_H__
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "math.h"
+#include "assert.h"
+
+#ifndef __FUNCTION_H__
+#define __FUNCTION_H__
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "math.h"
+#include "assert.h"
+
+#endif /* #define _FUNCTION_H__ */
+
+
+
+int enterchoice(void);
+
+
+void details(void);
+
+
+int ListofMovies(void);
+int movieselect(void);
+
+
+int changepriceticket(int);
+
+void show1(int choice,char name[10],int id1,int price);
+
+void show2(int choice,char name[10],int id1,int price);
+
+void show3(int choice,char name[10],int id1,int price);
+
+void reserveticket(int *,int,int );
+
+
+void cancelticket(int *); 
+
+
+#endif /* #define _FUNCTION_H__ */
