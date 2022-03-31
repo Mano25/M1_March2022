aa8a286cddc175b11e4ffc7fbaa8c504201d4759
Mano25 <101637522+Mano25@users.noreply.github.com>
2022-03-31 11:26:53 +0530

    first commit

---
+++ 3_implementation/src/cmovie.c
@@ -0,0 +1,12 @@
+#include "function.h"
+
+int movieselect(void)
+{
+	int i;
+	printf("\t\t\tMovie Name\n");
+	printf("\t\t\t-------------------------------------\n");
+	printf("\t\t\tpress 1 for Valimai\n\n");
+	printf("\t\t\tpress 2 for Beast\n\n");
+	scanf("%d",&i);
+	return i;
+}
